d3727e50dcbb31308705a1c53fae2587de591281
bayje8 <k.j.balachandar@gmail.com>
2020-04-17 22:23:51 -0400

    Updated

--- src/main/java/com/example/demo/QuadServicesController.java
+++ src/main/java/com/example/demo/QuadServicesController.java
@@ -5,29 +5,30 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 import org.apache.commons.io.FileUtils;
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class QuadServicesController {
 
-	@GetMapping("/service1")
+	@GetMapping("/int66")
 	public String getServiceOne() {
 		String serviceOne = "Default JSON response";
 		try {
-			serviceOne = FileUtils.readFileToString(new File(getClass().getClassLoader().getResource("ServiceOne.json").getFile()), StandardCharsets.UTF_8);
+			serviceOne = FileUtils.readFileToString(new File(getClass().getClassLoader().getResource("INT66.json").getFile()), StandardCharsets.UTF_8);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		}		
 		return serviceOne;
 	}
 	
-	@GetMapping("/service2")
+	@GetMapping("/int510")
 	public String getServiceTwo() {
 		String serviceTwo = "Default JSON response";
 		try {
-			serviceTwo = FileUtils.readFileToString(new File(getClass().getClassLoader().getResource("ServiceTwo.json").getFile()), StandardCharsets.UTF_8);
+			serviceTwo = FileUtils.readFileToString(new File(getClass().getClassLoader().getResource("INT510.json").getFile()), StandardCharsets.UTF_8);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -35,11 +36,11 @@ public class QuadServicesController {
 		return serviceTwo;
 	}
 	
-	@GetMapping("/service3")
+	@GetMapping("/int501")
 	public String getServiceThree() {
 		String serviceThree = "Default JSON response";
 		try {
-			serviceThree = FileUtils.readFileToString(new File(getClass().getClassLoader().getResource("ServiceThree.json").getFile()), StandardCharsets.UTF_8);
+			serviceThree = FileUtils.readFileToString(new File(getClass().getClassLoader().getResource("INT501.json").getFile()), StandardCharsets.UTF_8);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
